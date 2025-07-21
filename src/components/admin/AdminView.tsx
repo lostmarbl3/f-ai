c4b4741ecfeeed03099ebb3a49ae619550ec52ac
Lostmarbl3 <ermontgo@gmail.com>
2025-07-21 16:07:29 -0500

    Fix Button import in AdminView to use default export

--- src/components/admin/AdminView.tsx
+++ src/components/admin/AdminView.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Input from "@/components/ui/Input";
-import { Button } from "@/components/ui/Button";
+import Button from "@/components/ui/Button";
 
 type SetLog = {
   weight: number;
