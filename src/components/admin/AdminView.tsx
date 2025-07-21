b1f2092bc182d3e2df7ed59bc9b7ac0efaae32f5
Lostmarbl3 <ermontgo@gmail.com>
2025-07-21 15:52:41 -0500

    Remove orphaned workoutStore import from AdminView

--- src/components/admin/AdminView.tsx
+++ src/components/admin/AdminView.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useWorkoutStore } from "@/store/workoutStore";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 
